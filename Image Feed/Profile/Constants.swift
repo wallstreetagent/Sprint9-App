52dd5c3535d400174997a77116497da7818780e6
Yana V <velikanova6277@gmail.com>
2025-05-16 18:41:23 -0400

    no message

--- Image Feed/Profile/Constants.swift
+++ Image Feed/Profile/Constants.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "<ваш Access Key>"
-    static let secretKey = "<ваш Secret Key>"
-    static let redirectURI = "<ваш Redirect URI>"
+    static let accessKey = "GkDDtvymFKHbCwm_PaAZD2VrF3yCzhdy2AXkq2f1jcU"
+    static let secretKey = "KPPMv8Hgjw8zF_liQS4ZX6GELcYuDhFnHVq4WnNgu8A"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
 }
