c9eb7bff2dfb3e59e65e54669d046cea1011ca13
Yana V <velikanova6277@gmail.com>
2025-07-23 18:09:58 -0400

    no message

--- Image Feed/Auth/WebViewViewController.swift
+++ Image Feed/Auth/WebViewViewController.swift
@@ -50,8 +50,6 @@ final class WebViewViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // NOTE: Since the class is marked as `final` we don't need to pass a context.
-        // In case of inhertiance context must not be nil.
         webView.addObserver(
             self,
             forKeyPath: #keyPath(WKWebView.estimatedProgress),
